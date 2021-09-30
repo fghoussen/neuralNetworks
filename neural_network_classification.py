a4d7fda2634fb9279411425474e780db10f73dbc
Franck HOUSSEN <franck.houssen@safrangroup.com>
2021-09-30 13:22:35 +0200

    Neural network written from scratch: baseline version.

---
+++ neural_network_classification.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from sklearn.datasets import make_blobs
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from neural_network_library import network_train, network_predict, network_evaluate
+import matplotlib.pyplot as plt
+
+from random import seed
+seed(0) # Get repeatable results.
+
+def main():
+    # Create random data.
+    n_classes = 3
+    std_classes = 2 # Spread classes around centers to make data less/more predictable/noisy.
+    x, y = make_blobs(n_samples=1000, centers=n_classes, random_state=0, cluster_std=std_classes)
+    _, axes = plt.subplots(2, 5)
+    axes[0][0].scatter(x[:, 0], x[:, 1], c=y)
+    axes[0][0].set_title('data')
+    xlim, ylim = axes[0][0].get_xlim(), axes[0][0].get_ylim()
+
+    # Scale data to reduce weights.
+    std_scale = preprocessing.StandardScaler().fit(x)
+    x_scaled = std_scale.transform(x)
+    axes[0][1].scatter(x_scaled[:, 0], x_scaled[:, 1], c=y)
+    axes[0][1].set_title('scaled data (zoom unchanged)')
+    axes[0][1].set_xlim(xlim), axes[0][1].set_ylim(ylim)
+    axes[1][1].scatter(x_scaled[:, 0], x_scaled[:, 1], c=y)
+    axes[1][1].set_title('scaled data (zoom fit)')
+    xlim_scaled, ylim_scaled = axes[1][1].get_xlim(), axes[1][1].get_ylim()
+
+    # Split data set into training set, validation set and testing set.
+    x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.25)
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.25)
+
+    # Train neural network.
+    train_set = [[x_train[i, 0], x_train[i, 1], y_train[i]] for i in range(x_train.shape[0])]
+    val_set = [[x_val[i, 0], x_val[i, 1], y_val[i]] for i in range(x_val.shape[0])]
+    network, metrics = network_train(train_set, val_set, 2, 2, n_classes, 1000, 0.001)
+    predictions, error = network_evaluate(train_set, network)
+    axes[1][2].plot(metrics['train_error'], label='train error', marker='o')
+    axes[1][2].plot(metrics['val_error'], label='val error', marker='o')
+    axes[1][2].set_xlabel('epoch')
+    axes[1][2].set_title('learning curve SGD - error (%)')
+    axes[1][2].legend()
+    axes[1][3].scatter([ts[0] for ts in train_set], [ts[1] for ts in train_set], c=predictions)
+    axes[1][3].set_title('train SGD - error %.2f %%' % error)
+    axes[1][3].set_xlim(xlim_scaled), axes[1][3].set_ylim(ylim_scaled)
+
+    # Test neural network.
+    test_set = [[x_test[i, 0], x_test[i, 1], y_test[i]] for i in range(x_test.shape[0])]
+    predictions, error = network_evaluate(test_set, network)
+    axes[1][4].scatter([ts[0] for ts in test_set], [ts[1] for ts in test_set], c=predictions)
+    axes[1][4].set_title('test SGD - error %.2f %%' % error)
+    axes[1][4].set_xlim(xlim_scaled), axes[1][4].set_ylim(ylim_scaled)
+
+    # Show Results.
+    plt.show()
+
+if __name__ == '__main__':
+    main()
---
+++ neural_network_library.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from random import random
+from math import exp, log2
+
+def _initialize_network(n_inputs, n_hidden, n_outputs):
+    network = list()
+    hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)] # +1 for bias.
+    network.append(hidden_layer)
+    output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)] # +1 for bias.
+    network.append(output_layer)
+    print('network initialised')
+    for idxl, layer in enumerate(network):
+        print('    layer %s' % idxl)
+        for idxn, neuron in enumerate(layer):
+            print('        neuron %s' % idxn, neuron)
+    return network
+
+def _neuron_activate(weights, inputs):
+    activation = weights[-1] # Bias (associated input = 1.) is the last weight in the list.
+    for i in range(len(weights)-1): # Sum all but bias.
+        activation += weights[i] * inputs[i]
+    return activation
+
+def _sigmoid_transfer(activation):
+    return 1.0 / (1.0 + exp(-activation))
+
+def _sigmoid_transfer_derivative(output):
+    return output * (1.0 - output)
+
+def _forward_propagate(network, row):
+    inputs = row
+    for layer in network:
+        new_inputs = []
+        for neuron in layer:
+            activation = _neuron_activate(neuron['weights'], inputs)
+            neuron['output'] = _sigmoid_transfer(activation)
+            new_inputs.append(neuron['output'])
+        inputs = new_inputs
+    return inputs
+
+def _cross_entropy(p, q, eps=1e-15):
+    return -sum([p[i]*log2(q[i]+eps) for i in range(len(p))])
+
+def _backward_propagate_error(network, expected):
+    for i in reversed(range(len(network))): # Looping backward from output to hidden layer.
+        layer = network[i]
+        errors = list()
+        if i != len(network)-1: # Hidden layer.
+            for j in range(len(layer)):
+                error, next_layer = 0., network[i + 1]
+                for neuron in next_layer: # Looping over hidden layer output.
+                    error += (neuron['weights'][j] * neuron['delta'])
+                errors.append(error)
+        else: # Output layer.
+            for j in range(len(layer)):
+                neuron = layer[j]
+                errors.append(expected[j] - neuron['output'])
+        for j in range(len(layer)):
+            neuron = layer[j]
+            neuron['delta'] = errors[j] * _sigmoid_transfer_derivative(neuron['output'])
+
+def _update_weights(network, row, l_rate):
+    for i in range(len(network)):
+        inputs = row[:-1] # All but bias.
+        if i != 0:
+            prev_layer = network[i - 1] # Looping over hidden layer input.
+            inputs = [neuron['output'] for neuron in prev_layer]
+        for neuron in network[i]:
+            for j in range(len(inputs)):
+                neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
+            neuron['weights'][-1] += l_rate * neuron['delta'] # Bias (associated input = 1.) is the last weight in the list.
+
+def network_train(train_set, val_set, n_inputs, n_hidden, n_outputs, n_epoch, l_rate):
+    network = _initialize_network(n_inputs, n_hidden, n_outputs)
+    print('network training')
+    metrics = {'train_error': [], 'val_error': []}
+    for epoch in range(n_epoch):
+        sum_error = 0
+        for row in train_set:
+            outputs = _forward_propagate(network, row)
+            expected = [0 for i in range(n_outputs)]
+            expected[row[-1]] = 1
+            sum_error += _cross_entropy(expected, outputs)
+            _backward_propagate_error(network, expected)
+            _update_weights(network, row, l_rate)
+        print('    epoch=%d, lrate=%.3f, sum_error=%.3f' % (epoch, l_rate, sum_error))
+        _, error = network_evaluate(train_set, network)
+        metrics['train_error'].append(error)
+        _, error = network_evaluate(val_set, network)
+        metrics['val_error'].append(error)
+    return network, metrics
+
+def network_predict(network, row):
+    outputs = _forward_propagate(network, row)
+    return outputs.index(max(outputs))
+
+def network_evaluate(data_set, network):
+    predictions, good_predictions = [], 0
+    for row in data_set:
+        expected_row = row[-1]
+        predicted_row = network_predict(network, row)
+        if predicted_row == expected_row:
+            good_predictions += 1
+        predictions.append(predicted_row)
+    error = (len(data_set) - good_predictions)*100./len(data_set) # Error %
+    return predictions, error
